6cdfb6e15dad0d048463bdf7296be716c8d8d3bc
ostojics <ostojic.dev@gmail.com>
2022-06-11 21:42:34 +0200

    feat: add search

--- src/features/beers/beersApiSlice.ts
+++ src/features/beers/beersApiSlice.ts
@@ -1,4 +1,3 @@
-import {TagDescription} from '@reduxjs/toolkit/dist/query/endpointDefinitions';
 import {apiSlice} from 'features/api/apiSlice';
 import {GetBeersDTO} from 'typescript/dto/GetBeersDTO';
 import ApiData from 'typescript/interfaces/ApiData';
@@ -7,7 +6,7 @@ import {Beer} from 'typescript/models/Beer';
 export const beersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getBeers: builder.query<ApiData<Beer[]>, GetBeersDTO>({
-      query: (body) => `/beers?page=${body.page}&per_page=${body.perPage}`,
+      query: (body) => `/beers?page=${body.page}&per_page=${body.perPage}&filter[query_array]=${body.name || ''}`,
       // eslint-disable-next-line no-console
       transformResponse: (response: ApiData<Beer[]>) => {
         const {included} = response;
@@ -20,16 +19,17 @@ export const beersApiSlice = apiSlice.injectEndpoints({
 
         return response;
       },
-      providesTags: (result) => {
-        const tagArray: TagDescription<'Beer'>[] = [{type: 'Beer', id: 'LIST'}];
+      // providesTags: (result) => {
+      //   const tagArray: TagDescription<'Beer'>[] = [{type: 'Beer', id: 'LIST'}];
 
-        if (result) {
-          const beersTags: TagDescription<'Beer'>[] = result.data.map((beer) => ({type: 'Beer', id: beer.id}));
-          return tagArray.concat(beersTags);
-        }
+      //   if (result) {
+      //     const beersTags: TagDescription<'Beer'>[] = result.data.map((beer) => ({type: 'Beer', id: beer.id}));
+      //     return tagArray.concat(beersTags);
+      //   }
 
-        return tagArray;
-      },
+      //   return tagArray;
+      // },
+      keepUnusedDataFor: 0, // research more
     }),
   }),
 });
